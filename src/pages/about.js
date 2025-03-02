cbd1745903a06c869319bf49b5be27e5c02bf688
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-03-01 19:57:17 -0600

    Updated about page with metadata for SEO, imported components: Navbar, Header, MissionSection, Founder, ContactForm, GetInvolvedCTA, and Footer.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,37 @@
+import ContactForm from "@/components/ContactForm";
 import Footer from "@/components/Footer";
+import Founder from "@/components/Founder";
+import GetInvolvedCTA from "@/components/GetInvolvedCTA";
+import Header from "@/components/Header";
+import MissionSection from "@/components/MissionSection";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 
 export default function About() {
   return (
     <>
+      <Head>
+        <title>About Us</title>
+        <meta
+          name="description"
+          content="Messiah House Community Development Corporation non-profit mission and vision statements."
+          key="desc"
+        />
+      </Head>
       <Navbar />
-      <h1>About</h1>
+      <Header
+        title={"About Us"}
+        subtitle={"Serving with Purpose"}
+        description={`Messiah's House Community Development Corporation is dedicated to transforming lives through mentorship, financial literacy, housing support, and education.`}
+      />
+
+      <MissionSection />
+      <Founder />
+
+      <div id="contact" className="bg-base-200 p-6">
+        <ContactForm />
+      </div>
+      <GetInvolvedCTA />
       <Footer />
     </>
   );
