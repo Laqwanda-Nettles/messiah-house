d9d6740c3a4ec8b1ddae6100f9d289a7d8f0a3e9
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-03-12 02:49:29 -0500

    Add register page with RegisterForm, Navbar, and Footer components

---
+++ src/pages/register.js
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import RegisterForm from "@/components/RegisterForm";
+
+export default function Register() {
+  return (
+    <>
+      <Navbar />
+      <RegisterForm />
+      <Footer />
+    </>
+  );
+}
