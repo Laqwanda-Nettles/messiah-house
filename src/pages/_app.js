e9565d67137ccbeeb808bcfc94aafe5c1b7355dd
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-03-12 02:44:50 -0500

    Wrap application with AuthProvider and apply global font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
+import { AuthProvider } from "@/context/AuthProvider";
 import "@/styles/globals.css";
 import { Parkinsans } from "next/font/google";
 const parkinsans = Parkinsans({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={parkinsans.className}>
-      <Component {...pageProps} />
-    </main>
+    <AuthProvider>
+      <main className={parkinsans.className}>
+        <Component {...pageProps} />
+      </main>
+    </AuthProvider>
   );
 }
