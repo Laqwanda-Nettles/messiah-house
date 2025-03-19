d6affb220bc5e23dcd31fd94bfddf8f855ccce0c
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-03-19 17:21:08 -0500

    Update admin portal dashboard with authentication (ProtectedRoute), LatestBlogs component, and SEO meta & title

--- src/pages/admin/portal/index.js
+++ src/pages/admin/portal/index.js
@@ -1,26 +1,43 @@
 import AdminNavbar from "@/components/AdminNavbar";
+import LatestBlogs from "@/components/LatestBlogs";
+import ProtectedRoute from "@/components/ProtectedRoute";
 import { useAuth } from "@/context/AuthProvider";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Portal() {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const router = useRouter();
 
   useEffect(() => {
-    if (!user) {
+    if (!loading && !user) {
       router.push("/login");
     }
-  }, [user, router]);
+  }, [user, loading, router]);
 
-  if (!user) return <p>Loading...</p>;
+  if (loading) return <p>Loading...</p>;
+  if (!user) return null;
 
   return (
-    <div className="flex h-screen">
-      <AdminNavbar />
-      <div className="flex-1 overflow-y-auto">
-        <h1>Welcome, {user.email}!</h1>
+    <ProtectedRoute>
+      <Head>
+        <title>MHCDC Portal | Dashboard</title>
+        <meta
+          name="description"
+          content="Messiah House Community Development Corporation non-profit portal dashboard home page"
+          key="desc"
+        />
+      </Head>
+      <div className="flex h-screen">
+        <AdminNavbar />
+        <div className="flex-1 overflow-y-auto m-4">
+          <div className="bg-emerald-100 p-4 rounded shadow-md mb-4">
+            <h1 className="text-xl font-semibold">Welcome, {user.email}!</h1>
+          </div>
+          <LatestBlogs />
+        </div>
       </div>
-    </div>
+    </ProtectedRoute>
   );
 }
