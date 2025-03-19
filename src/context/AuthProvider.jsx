10476111d8f771696a493022b8ae6a8e6d077631
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-03-19 17:18:45 -0500

    Implement AuthProvider context with token retrieval, logout handling, and Firebase authentication

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,21 +1,47 @@
 import { auth } from "@/config/firebase";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, signOut, getIdToken } from "firebase/auth";
+import { useRouter } from "next/router";
 import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      if (currentUser) {
+        try {
+          const token = await getIdToken(currentUser, true);
+          localStorage.setItem("token", token);
+          setUser(currentUser);
+        } catch (error) {
+          console.error("Token refresh error: ", error);
+          handleLogout();
+        }
+      } else {
+        handleLogout();
+      }
+
+      setLoading(false);
     });
+
     return () => unsubscribe();
-  }, [auth]);
+  }, []);
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    localStorage.removeItem("token");
+    setUser(null);
+    router.push("/login");
+  };
 
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, loading, handleLogout }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
 
