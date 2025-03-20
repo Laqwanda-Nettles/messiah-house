ae06abb6b61669de8c58da5f4d35198055e25b5b
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-03-20 15:05:21 -0500

    Create MeetDeveloper section component

---
+++ src/components/MeetDeveloper.jsx
@@ -0,0 +1,46 @@
+export default function MeetDeveloper() {
+  return (
+    <>
+      <h2 className="text-4xl text-secondary font-bold divider divider-warning mt-6">
+        Meet the Developer
+      </h2>
+      <div className="m-5 text-xl">
+        <h3 className="font-medium text-2xl text-zinc-600 text-center mb-4">
+          <span className="font-semibold text-info">Laqwanda Nettles</span> —
+          Junior Full Stack Developer
+        </h3>
+        <p className="mb-3">
+          Laqwanda Nettles is a Junior Full Stack Developer and a recent
+          graduate of CodeX Academy, bringing a unique background in culinary
+          arts and caregiving to the world of technology. Passionate about
+          creativity and problem-solving, she enjoys designing and building
+          innovative digital experiences, including this nonprofit website.
+        </p>
+        <p className="mb-3">
+          Although new to the tech industry, she is eager to gain hands-on
+          experience, expand her knowledge, and collaborate on impactful
+          projects. With a strong commitment to learning and growth, she looks
+          forward to connecting with others and making meaningful contributions
+          in the field.
+        </p>
+        <p>
+          Explore her work on{" "}
+          <a
+            href="https://github.com/Laqwanda-Nettles"
+            className="link link-hover link-info"
+          >
+            GitHub
+          </a>{" "}
+          or connect with her on{" "}
+          <a
+            href="https://www.linkedin.com/in/laqwanda-nettles-b8630734b"
+            className="link link-hover link-info"
+          >
+            LinkedIn
+          </a>
+          !
+        </p>
+      </div>
+    </>
+  );
+}
